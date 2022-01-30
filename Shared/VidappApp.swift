bf9f4c92421ffc33a7d5795b8de101302b6f4528
Manny C <mannychau@gmail.com>
2022-01-30 01:54:53 +0000

    Initial Commit

---
+++ Shared/VidappApp.swift
@@ -0,0 +1,17 @@
+//
+//  VidappApp.swift
+//  Shared
+//
+//  Created by YukiseKimi on 30/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct VidappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
